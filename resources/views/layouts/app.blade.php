c048a39a921e5cf0f75792081ea2beb8d95b38ec
khalidesskali <esskalikhalid7@gmail.com>
2025-03-06 14:24:15 +0000

    set up header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,8 @@
             @endisset
 
             <!-- Main Content Area -->
-            <main class="py-12">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                        {{ $slot }}
-                    </div>
-                </div>
+            <main>
+                @yield('content')
             </main>
 
         </div>
